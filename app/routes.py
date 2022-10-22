a1e01cc8b1394d24b68d592137489cc525e895fc
Tiffany Tran <tif.tran86@gmail.com>
2022-10-22 16:59:21 -0700

    completed 02a-read-one-book

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
         self.id = id
         self.title = title
         self.description = description
+
+def validate_books(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message": f"book {book_id} not found"}, 404))
 
 books = [
     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
@@ -30,11 +42,9 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+    book = validate_books(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
