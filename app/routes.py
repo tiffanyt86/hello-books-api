a82acdf60a308b0dd3ffa8b54a46ed1964f47ed1
Tiffany Tran <tif.tran86@gmail.com>
2022-10-31 22:02:50 -0700

    Completed 03c-read-all-books

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
@@ -29,26 +29,24 @@ def read_all_books():
         )
     return jsonify(books_response)
 
-#def validate_book(book_id):
-#    try:
-#        book_id = int(book_id)
-#    except:
-#        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-#
-#    for book in books:
-#        if book.id == book_id:
-#            return book
-#
-#    abort(make_response({"message":f"book {book_id} not found"}, 404))
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+       abort(make_response({"message":f"book {book_id} invalid"}, 400))
+       
+    book = Book.query.get(book_id)
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
         
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-#
-#     return {
-#           "id": book.id,
-#           "title": book.title,
-#           "description": book.description,
-#     }
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
 
-
+    return {
+          "id": book.id,
+          "title": book.title,
+          "description": book.description,
+    }
