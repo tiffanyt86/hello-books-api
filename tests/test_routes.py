aebaaa3bab3d87835c75c4e8264cc80b61da63b2
Tiffany Tran <tif.tran86@gmail.com>
2022-11-02 21:33:56 -0700

    completed 06c-using-tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,4 @@
+# get all books and return no records
 def test_get_all_books_with_no_records(client):
     # Act
     response = client.get("/books")
@@ -6,3 +7,17 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+# get one book by id
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
