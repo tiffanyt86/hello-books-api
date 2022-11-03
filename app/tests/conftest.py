5e3b23b7f5328324b431e0b7408bd55acb856e8c
Tiffany Tran <tif.tran86@gmail.com>
2022-11-02 21:13:24 -0700

    completed 06a-test-database

---
+++ app/tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finished
+
+
+@pytest.fixture
+def app():
+    # flags create_app to go into the testing environment
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    # creates an empty database for us to test on
+    with app.app_context():
+        db.create_all()
+        yield app # tells our app to create an instance and sends it to the test to do its 
+                    # thing and comes back here
+
+    # once other tests are complete, it returns and clears out any context that was added
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
